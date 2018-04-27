38e734762813beed65daa8cd5208cd7c83b9e0ab
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-27 13:53:04 +0200

    Ajout CreateProperties

---
+++ src/main/java/fr/pizzeria/jdbcproperties/CreateProperties.java
@@ -0,0 +1,39 @@
+package fr.pizzeria.jdbcproperties;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
+public class CreateProperties {
+	public static void main(String[] args) {
+
+		Properties prop = new Properties();
+		OutputStream output = null;
+
+		try {
+
+			output = new FileOutputStream("config_jdbc.properties");
+
+			// set the properties value
+			prop.setProperty("url", "jdbc:mysql://127.0.0.1/pizzadb");
+			prop.setProperty("user", "root");
+			prop.setProperty("password", "");
+
+			// save properties to project root folder
+			prop.store(output, null);
+
+		} catch (IOException io) {
+			io.printStackTrace();
+		} finally {
+			if (output != null) {
+				try {
+					output.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+}

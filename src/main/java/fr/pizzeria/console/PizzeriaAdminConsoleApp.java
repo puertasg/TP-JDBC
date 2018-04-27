27b571c49133e79d0d7f4870082050eb38c53eb8
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-27 15:58:42 +0200

    Formatage PizzeriaAdminConsole

--- src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -7,17 +7,16 @@ import fr.pizzeria.services.MenuService;
 import fr.pizzeria.services.MenuServiceFactory;
 
 public class PizzeriaAdminConsoleApp {
-	
+
 	public static void main(String[] args) {
-		
-		
+
 		/* Initialisation du TDB de pizzas */
 		PizzaMemDaoJdbc dao = new PizzaMemDaoJdbc();
-		
+
 		/* Initialisation du scanner */
 		Scanner scanner = new Scanner(System.in);
 		int choix = 0;
-		
+
 		/* Structure de r�p�tition de la boucle principale */
 		do {
 			println("***** Pizzeria Administration *****");
@@ -27,22 +26,20 @@ public class PizzeriaAdminConsoleApp {
 			println("4. Supprimer une pizza ");
 			println("5. Initialiser la base de données ");
 			println("99. Sortir ");
-	
+
 			System.out.print("Veuillez choisir une option:");
 			choix = scanner.nextInt();
-			
+
 			MenuService service = MenuServiceFactory.getInstance(choix);
 			service.executeUC(scanner, dao);
-			
-		} while(choix!=99);
-		
+
+		} while (choix != 99);
+
 		scanner.close();
 		dao.close();
 	}
-	
-	
 
-	private static void println(String msg){
+	private static void println(String msg) {
 		System.out.println(msg);
 	}
 }

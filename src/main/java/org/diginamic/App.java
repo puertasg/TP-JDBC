04649f9036a8a800c3a5c03429739f05b2566014
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-27 11:08:04 +0200

    Exécution requete, passage à un bloc try

--- src/main/java/org/diginamic/App.java
+++ src/main/java/org/diginamic/App.java
@@ -9,17 +9,27 @@ import java.sql.Statement;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        try {
-			Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/bdd_mysql","root","");
+public class App {
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/pizzadb", "root", "");
 			Statement statement = connection.createStatement();
+
+			String sqlCreate = "CREATE TABLE pizzas " 
+					+ "(id INTEGER not NULL, " 
+					+ " CODE VARCHAR(3), "
+					+ " LIBELLE VARCHAR(255), " 
+					+ " PRIX DOUBLE, " 
+					+ " PRIMARY KEY ( id ))";
+
+			statement.executeQuery(sqlCreate);
 		} catch (SQLException e) {
-			
+			System.out.println("Echec de l'éxécution de la requete SQL");
+		} catch (ClassNotFoundException e1) {
+			System.out.println("Echec du chargement du driver");
+			e1.printStackTrace();
 		}
-        
-        
-    }
+
+	}
 }

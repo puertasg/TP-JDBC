d543455c85487f311e058879092a830d48e6c4f0
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-27 10:50:51 +0200

    Instanciation objet Connection

--- src/main/java/org/diginamic/App.java
+++ src/main/java/org/diginamic/App.java
@@ -1,5 +1,9 @@
 package org.diginamic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/bdd_mysql","root","");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
